e3a94df955e64d387e08daa536a32b21885a56c0
Karl Price <karuprice@gmail.com>
2016-08-18 16:27:47 +0100

    events page renders

--- controllers/events.js
+++ controllers/events.js
@@ -1,15 +1,97 @@
+var Seasons = require('../models/seasons');
 var Events = require('../models/events');
+var Circuits = require('../models/circuits');
+var async = require('async');
 
 module.exports = {
-  getEvents: getEvents
+  getAllEvents: getAllEvents
 };
 
-function getEvents(query, callback) {
-  console.log('Called getEvents controller');
-  Events.find(query, function (err, events) {
-    if (err) {
-      return callback(err);
+function getAllEvents (from, until, finalCallback) {
+  // go and find all the events where the date_time
+  var query = {};
+  if (from && until) {
+    // get tome stamps from params
+    // check they are valid
+    query.date_time = {$gt: from, $lt: until};
+  }
+  async.waterfall([
+    function (callback) {
+      Events.find(query, function (err, events) {
+        // now pass all the events down our waterfall
+        callback(null, events);
+      })
+    },
+    function (events, callback) {
+      // get the year for the events and modify the doc
+      // hold that season id in memory as we have many rounds to a season and do not need the db every time
+      var season_id = '';
+      var year = '';
+      var returnEvents = [];
+      var newEvent;
+      // async eachSeries on our events
+      async.eachSeries(events, function (event, eventCallback) {
+        // check the id is the id if so add the year,
+        event = event.toObject();
+        if (event.season_id === season_id) {
+          newEvent = {
+            date_time: event.date_time,
+            season: year,
+            round: event.round,
+            circuit_id: event.circuit_id
+          };
+          return eventCallback();
+        }
+        Seasons.findById(event.season_id, function (error, season) {
+          // otherwise do a db fetch, add the year and then reset the variables
+          if (error) {
+            return callback(error);
+          }
+          season = season.toObject();
+          season_id = season._id;
+          year = season.season;
+          newEvent = {
+            date_time: event.date_time,
+            season: year,
+            round: event.round,
+            circuit_id: event.circuit_id
+          };
+          returnEvents.push(newEvent);
+          eventCallback();
+        });
+      }, function () {
+        // callback with all our modified events
+        callback(null, returnEvents);
+      });
+    },
+    function (events, callback) {
+      // now we just need the circuit for that event
+      // we should also create a 'map' for our circuits so we don't do db look ups and the circuits are usually used more than once
+      var circuitsMap = {};
+
+      // again lets us a eachSeries
+      async.eachSeries(events, function (event, eventCallback) {
+        if (circuitsMap[event.circuit_id]){
+          event.circuit = circuitsMap[event.circuit_id];
+          return eventCallback();
+        }
+        // otherwise do a db fetch, add the year and add it to the map
+        Circuits.findById(event.circuit_id, function (error, circuit) {
+          if (error) {
+            return callback(error);
+          }
+          circuit = circuit.toObject();
+          event.circuit = circuit;
+          circuitsMap[event.circuit_id] = circuit;
+          eventCallback();
+        });
+      }, function () {
+        callback(null, events);
+      })
     }
-    callback(null, events)
-  })
-};
+  ], function (error, events) {
+    finalCallback(null, events);
+  });
+
+
+}
--- routes/views.js
+++ routes/views.js
@@ -2,6 +2,32 @@ var express = require('express');
 var router = express.Router();
 // extract out our logic that controls each route into our controllers
 var seasonsController = require('../controllers/seasons');
+var eventsController = require('../controllers/events');
+
+
+router.get('/events', function (req, res) {
+  // we call the controller to get all eventss
+  var from, until;
+  if (req.query.from && req.query.until) {
+    var fromDate = new Date(req.query.from).getTime();
+    var untilDate = new Date(req.query.until).getTime();
+    from = fromDate ? fromDate : null;
+    until = untilDate ? untilDate : null;
+  }
+  eventsController.getAllEvents(from,until, function (error, data) {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    var ejsData = {
+      title: 'Events',
+      allEvents: data
+    };
+
+    res.status(200).render('pages/events', ejsData);
+  })
+});
+
+
 
 router.get('/seasons', function (req, res) {
   // we call the controller to get all seasons
